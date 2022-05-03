15491647f2c1b01ac05818c3f065be13d8175304
Sam <snippetsbysam@outlook.com>
2022-05-04 00:22:20 +0100

    Exercise001 complete

--- Exercises/Exercise001.cs
+++ Exercises/Exercise001.cs
@@ -9,31 +9,54 @@ namespace Exercises
         public string CapitalizeWord(string word)
         {
             // Replace the exception statement below with your code!
-            throw new NotImplementedException();
+            if(word.Length > 0)
+            {
+                return $"{char.ToUpper(word[0])}{word[1..]}";
+            }
+            // If string is empty
+            return string.Empty;
         }
 
         public string GenerateInitials(string firstName, string lastName)
         {
             // Replace the exception statement below with your code!
-            throw new NotImplementedException();
+            return $"{firstName[0]}.{lastName[0]}";
         }
 
         public double AddVat(double originalPrice, double vatRate)
         {
             // Replace the exception statement below with your code!
-            throw new NotImplementedException();
+            return Math.Round(originalPrice * ( 1 + (vatRate / 100)), 2);
         }
 
         public string Reverse(string sentence)
         {
             // Replace the exception statement below with your code!
-            throw new NotImplementedException();
+            if(sentence.Length > 0)
+            {
+                string reversed = "";
+                for (int i = sentence.Length - 1; i > -1; i--)
+                {
+                    reversed += sentence[i];
+                }
+                return reversed;
+            };
+            // Empty sentence
+            return string.Empty;
         }
 
         public int CountLinuxUsers(List<User> users)
         {
             // Replace the exception statement below with your code!
-            throw new NotImplementedException();
+            int linuxUsers = 0;
+            foreach (User user in users)
+            {
+                if (user.Type =="Linux")
+                    {
+                        linuxUsers++;
+                    }
+            }
+            return linuxUsers;
         }
     }
 }
