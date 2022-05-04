a111fc2a7f8827ac022f9f5603fa0bfaa1150a8e
Sam <snippetsbysam@outlook.com>
2022-05-04 20:31:36 +0100

    Exercise002 complete

--- Exercises/Exercise002.cs
+++ Exercises/Exercise002.cs
@@ -1,18 +1,18 @@
 ﻿using System;
-namespace Exercises
+namespace Exercises.Models
 {
     public class Exercise002
     {
-        public bool IsFromManchester(/* Person person */)
+        public bool IsFromManchester( Person person)
         {
             // Replace the exception statement below with your code!
-            throw new NotImplementedException();
+            return (person.City == "Manchester");
         }
 
-        public bool CanWatchFilm(/*Person person, int ageLimit*/)
+        public bool CanWatchFilm(Person person, int ageLimit)
         {
             // Replace the exception statement below with your code!
-            throw new NotImplementedException();
+            return (person.Age >= ageLimit);
         }
     }
 }
--- Exercises/Models/User.cs
+++ Exercises/Models/User.cs
@@ -17,4 +17,21 @@ namespace Exercises.Models
 
         public string Type { get; set; }
     }
+
+    public class Person
+    {
+        public Person(string firstName, string lastName, string city, int age)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            City = city;
+            Age = age;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+        public int Age { get; set; }
+
+    }
 }
