cf762b8ecc1b3e3de5ee716b8dd0f7d8c96028d4
Sam <snippetsbysam@outlook.com>
2022-05-05 18:47:56 +0100

    Exercise005 complete

--- Exercises/Exercise005.cs
+++ Exercises/Exercise005.cs
@@ -6,7 +6,21 @@ namespace Exercises
         public bool IsPangram(string input)
         {
             // Replace the exception statement below with your code!
-            throw new NotImplementedException();
+            // counter for letters found
+            int lettersFound = 0;
+            string alphabet = "abcdefghijklmnopqrstuvwxyz";
+            // set the input to lower case
+            string phrase = input.ToLower();
+            foreach(char letter in alphabet)
+            {
+                if (phrase.IndexOf(letter) > -1)
+                {
+                    // add 1 to number of letters found
+                    lettersFound++;
+                }
+            }
+            // returns true if all 26 letters are found, otherwise false
+            return lettersFound == 26;
         }
     }
 }
