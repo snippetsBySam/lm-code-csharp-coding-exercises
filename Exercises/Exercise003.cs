99a59356504619cdb9765826dd2e12c5ea7560e3
Sam <snippetsbysam@outlook.com>
2022-05-05 17:55:54 +0100

    Exercise003 complete

--- Exercises/Exercise003.cs
+++ Exercises/Exercise003.cs
@@ -3,10 +3,17 @@ namespace Exercises
 {
     public class Exercise003
     {
+        string[] PipStore = { "Pistachio", "Raspberry Ripple", "Vanilla", "Mint Chocolate Chip", "Chocolate", "Mango Sorbet" };
         // Replace the exception statement below with your code!
-        public string[] IceCreamFlavours => throw new NotImplementedException();
+        public string[] IceCreamFlavours
+        {
+            get { return PipStore; }
+        }
 
         // Replace the exception statement below with your code!
-        public int IceCreamCode(string iceCreamFlavour) => throw new NotImplementedException();
+        public int IceCreamCode(string iceCreamFlavour)
+        {
+            return Array.IndexOf(PipStore, iceCreamFlavour);
+        }
     }
 }
